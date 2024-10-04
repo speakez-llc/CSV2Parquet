6b04c02c846d8c56226cb75725bd04561cb15da5
Houston Haynes <8174976+houstonhaynes@users.noreply.github.com>
2024-10-03 21:26:09 -0400

    Create cms_view

--- src/CSV2Parquet/Files/create_cms_view.sql
+++ src/CSV2Parquet/Files/create_cms_view.sql
@@ -1,2 +1,13 @@
-CREATE VIEW "CMS_VIEW" AS 
-    
+CREATE OR REPLACE VIEW "CMS_VIEW" AS 
+SELECT 
+    *,
+    Total_Drug_Cost / Total_Day_Supply AS Total_Drug_Cost_Per_Day,
+    Total_Drug_Cost_65_OrOlder / Total_Day_Supply_65_OrOlder AS Total_Drug_Cost_Per_Day_65_OrOlder,
+    Total_Drug_Cost / Total_Claims AS Total_Cost_Per_Claim,
+    Total_Drug_Cost_65_OrOlder / Total_Day_Supply_65_OrOlder AS Total_Cost_Per_Claim_65_OrOlder,
+    Total_Drug_Cost / Total_Beneficiaries AS Total_Cost_Per_Beneficiary,
+    Total_Drug_Cost_65_OrOlder / Total_Beneficiaries_65_OrOlder AS Total_Cost_Per_Beneficiary_65_OrOlder,
+    Total_30Day_Fills / Total_Beneficiaries AS Total_30_Day_Fills_Per_Beneficiary,
+    Total_30Day_Fills_65_OrOlder / Total_Beneficiaries_65_OrOlder AS Total_30_Day_Fills_Per_Beneficiary_65_OrOlder
+FROM 
+    CMS;
